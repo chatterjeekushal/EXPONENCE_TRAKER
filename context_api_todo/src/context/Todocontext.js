ab542197cd5792099ed1d9c9ac365c34e3b0400b
chatterjeekushal <chatterjeekushal89@gmail.com>
2024-05-03 11:49:51 +0530

    add new project

---
+++ context_api_todo/src/context/Todocontext.js
@@ -0,0 +1,27 @@
+
+import React from "react";
+
+import { createContext,useContext } from "react";
+
+export const Todocontext=createContext({
+    todos:[
+
+        {
+            id:1,
+            todo:"todo msg",
+            completed:false,
+        }
+    ],
+
+    addTodo:(todo)=>{},
+    updateTodo:(id,todo)=>{},
+    deleteTodo:(id)=>{},
+    togglecomplete:(id)=>{}
+})
+
+export const useTodo=()=>{
+
+    return useContext(Todocontext)
+}
+
+export const Todoprovider=Todocontext.Provider
